1a7e009eb133ad6cbe390aa762e831d27d7ad19f
EliederSousa <eliedersousa@gmail.com>
2020-10-07 08:29:45 -0300

    Update Tutorial - circulo.glsl

--- Tutorial - circulo.glsl
+++ Tutorial - circulo.glsl
@@ -23,26 +23,23 @@ uniform vec2 resolution;
 void main(void) {
     // Guarda o tamanho da tela (width, height)
     vec2 iResolution = vec2(resolution.x , resolution.y);
-    // FragCoord  um vec2 que aponta pro pixel
-    // Logo, essa diviso normaliza a entrada
-    // Pois ao invs de trabalhar com a posio do
-    // Pixel atual, trabalhamos com um intervalo fechado [0,1]
+    // FragCoord é um vec2 que aponta para o pixel
+    // Logo, essa divisão normaliza a entrada pois ao invés de trabalhar com a posição do pixel atual, trabalhamos com um intervalo fechado [0,1]
     vec2 uv = gl_FragCoord.xy / iResolution.xy;
 
-    // uv  como o plano cartesiano; {0,0}  o canto inferior esquerdo
-    // podemos fazer o centro do plano ser o centro da tela
+    // uv é como o plano cartesiano; {0,0} é o canto inferior esquerdo. Podemos fazer o centro do plano ser o centro da tela.
     // Como uv.x ou .y vai de 0 a 1, basta subtrair .5 de x e y.
     uv -= .5;
+    // Aqui, acertamos o aspect ratio (pois telas diferentes tem tamanhos de largura diferentes da altura).
     uv.x *= iResolution.x / iResolution.y;
 
-    // O raio do crculo
+    // O raio do círculo
     float radius = 0.2;
-    // d ser a distncia da origem do plano {0,0} para o pixel atual
-    // que no caso pode ser qualquer coisa entre -.5 e .5 (tanto x como y)
+    // d é a distância da origem do plano {0,0} para o pixel atual que no caso pode ser qualquer coisa entre -.5 e .5 (tanto x como y)
     float d = length(uv);
-    // define a cor pela funo smoothstep.
+    // Define a cor pela funo smoothstep.
     float c = smoothstep( radius, radius - .01, d);
-    // Poderiamos desenhar um circulo assim tambm
+    // Poderíamos desenhar um circulo assim também
     //if(d < 0.01) { c = 1.0; } else { c = 0.; }
 
     // Envia a cor para a tela.
