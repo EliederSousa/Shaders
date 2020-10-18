1e8f35e144983bd0ce370b25bf976c9c0fec67d0
EliederSousa <eliedersousa@gmail.com>
2020-10-18 17:34:26 -0300

    Update Light Attenuation.glsl

--- Light Attenuation.glsl
+++ Light Attenuation.glsl
@@ -14,27 +14,27 @@
  */
  
 vec4 LightPoint( vec2 uv, vec2 pos, vec3 color, vec3 att ) {
- 	float dist = length ( pos - uv ) * 1.;
-    float at = 1. / (att.x + att.y * dist + att.z * dist * dist );
-    return vec4( at, at, at, 1. ) * vec4 ( color, 1. );
+	float dist = length ( pos - uv ) * 1.;
+	float at = 1. / (att.x + att.y * dist + att.z * dist * dist );
+	return vec4( at, at, at, 1. ) * vec4 ( color, 1. );
 }
 
 vec2 MousePosition() {
- 	return vec2((iMouse.x / iResolution.x) - .5, (iMouse.y / iResolution.y) - .5);
+	return vec2((iMouse.x / iResolution.x) - .5, (iMouse.y / iResolution.y) - .5);
 }
 
 float rand(vec2 co){
-    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+	return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord ){
-    vec2 uv = fragCoord / iResolution.xy;
-    uv -= .5;
-    uv.x *= iResolution.x / iResolution.y;
-    
-    vec4 color = vec4(.0);      
-    color += LightPoint( uv, vec2(0., 0.), vec3(1.0, 1.0, 1.0), vec3(0., 50., 500.) );
-    color += LightPoint( uv, MousePosition(), vec3(.8, .7, .3), vec3(0., 100., 500.) );
+	vec2 uv = fragCoord / iResolution.xy;
+	uv -= .5;
+	uv.x *= iResolution.x / iResolution.y;
+
+	vec4 color = vec4(.0);      
+	color += LightPoint( uv, vec2(0., 0.), vec3(1.0, 1.0, 1.0), vec3(0., 50., 500.) );
+	color += LightPoint( uv, MousePosition(), vec3(.8, .7, .3), vec3(0., 100., 500.) );
     
 	fragColor = color;
 }
